a63af4d7a89626b00262e56fb9530fa49b35feff
Thanh Tu <lethanhtu18031999@gmail.com>
2021-10-20 22:08:03 +0700

    Fix

--- difiaz_space/lib/main.dart
+++ difiaz_space/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:difiaz_space/themes.dart';
 import 'package:difiaz_space/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
@@ -7,12 +8,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'helpers/colors.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
       ChangeNotifierProvider(
         create: (_) => AppTheme(),
-        child:  MyApp(),
+        child:  const MyApp(),
       )
   );
   configLoading();
